01ccea0f8b880118500c997f0bfaeef15d357804
Daniel_Thevening <gonfidaniel@gmail.com>
2024-04-28 12:29:56 -0500

    firs commit

---
+++ src/main/java/com/example/Cuentalo/CuentaloApplication.java
@@ -0,0 +1,13 @@
+package com.example.Cuentalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentaloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Cuentalo/CuentaloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cuentalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
